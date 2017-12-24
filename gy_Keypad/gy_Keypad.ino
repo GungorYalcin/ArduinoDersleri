9e145c5179e02df1f8f36d61866821c2e1fc8edd
Gungor Yalcin <gungoryalcin68@gmail.com>
2017-12-24 13:18:03 +0300

    1
    1

---
+++ gy_Keypad/gy_Keypad.ino
@@ -0,0 +1,34 @@
+/*
+gungor 4x4 KeyPad kullanımı
+*/
+#include <Keypad.h>
+const byte satir = 4;
+const byte sutun = 4;
+
+char tus_tanim[satir][sutun] = {
+{'1','2','3','A'},
+{'3','4','5','B'},
+{'6','7','8','C'},
+{'*','0','#','D'}
+};
+byte satir_pin[satir] = {9,8,7,6};
+byte sutun_pin[sutun] = {5,4,3,2};
+Keypad tus_takimi = Keypad(makeKeymap(tus_tanim),satir_pin,sutun_pin,satir,sutun);
+char tus;
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+  tus = tus_takimi.getKey();
+  if (tus)
+  { 
+    Serial.println(tus);
+    if (tus ==  '#') 
+    {
+      Serial.println("# basıldı");
+    }
+  }
+   delay(100);
+}
