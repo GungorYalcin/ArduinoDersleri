280a6862b7f70972ce7298b69d997164c4b6dfdf
Gungor YALCIN <gungor.yalcin@paximum.com>
2017-12-28 12:06:51 +0300

    1
    1

---
+++ gy_Ethernet_Role/role.php
@@ -0,0 +1,61 @@
+<?php
+  require_once("baglanti.php");
+  global $baglanti;
+
+ $ip = trim($_SERVER["REMOTE_ADDR"]);
+ $Tarih = date("Y-m-d h:i:s");  
+
+   if (isset($_GET['ID']))
+  {
+    $KartID =$_GET['ID'];
+  }
+
+  if (isset($_POST['ID']))
+  {
+    $KartID =$_POST['ID'];
+  }
+
+  if (isset($_GET['value']))
+  {
+    $deger =$_GET['value'];
+  }
+
+  if (isset($_POST['value']))
+  {
+    $deger =$_POST['value'];
+  }
+ 
+     $query = "select ID from DP_Kart where ID='{$KartID}' ";
+     $subject_set = sqlsrv_query( $baglanti,$query);
+     $subject2 = sqlsrv_fetch_array($subject_set,SQLSRV_FETCH_ASSOC);
+      if ($subject2['ID']==$KartID )
+      { 
+         $query = "Update DP_Kart set DEGERSTR= '{$deger}', chg_date='{$Tarih}' where ID ={$KartID} ";
+      }
+      else 
+      {
+         $query = "insert into DP_Kart (ID,IP,TIPI,SIRA,DEGERSTR,chg_date) values ('{$KartID}','{$ip}',2,0,'{$deger}','{$Tarih}')";
+	  }
+	 $stmt = sqlsrv_prepare($baglanti,$query,array());
+        if( sqlsrv_execute( $stmt ) === false ) 
+            echo "hata:",sqlsrv_errors();
+
+$query = "insert into DP_Log (ID,IP,TIPI,SIRA,DEGERSTR,chg_date) values ('{$KartID}','{$ip}',2,0,'{$deger}','{$Tarih}')";
+$stmt = sqlsrv_prepare($baglanti,$query,array());
+        if( sqlsrv_execute( $stmt ) === false ) 
+            echo "hata:",sqlsrv_errors();
+		
+  sqlsrv_close($baglanti);
+  
+/*
+if (file_exists("./users/role.txt"))
+{
+ $islem=fopen("./users/role.txt","a");
+  $metin = " Tarih=".date("d/m/y H:i")." IP=".rtrim($_SERVER["REMOTE_ADDR"])." ID:".$KartID." Value:".$deger." XXX\n";
+  fputs($islem,$metin);
+  fclose($islem);
+}
+*/
+  
+echo "OK",$KartID,$deger,"<<<",$subject2['ID'],"<<<";
+?>
