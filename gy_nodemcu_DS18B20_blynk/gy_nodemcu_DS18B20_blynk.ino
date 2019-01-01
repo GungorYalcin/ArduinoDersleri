356caa3f8ed2cdf9348348d3bd2692ec21e9c85d
Gungor Yalcin <gungoryalcin68@gmail.com>
2019-01-01 20:40:58 +0300

    1
    1

--- gy_nodemcu_DS18B20_blynk/gy_nodemcu_DS18B20_blynk.ino
+++ gy_nodemcu_DS18B20_blynk/gy_nodemcu_DS18B20_blynk.ino
@@ -12,8 +12,11 @@ char auth[] = "ceecb258c5ef4db085dbb24393ea131f";
 char ssid[] = "Lavender";
 char pass[] = "5335921602";
 
+float  temp = 0;
 float SonMesaj = 0;
 float SonEMail = 0;
+int MesajGonder = 0;
+int EMailGonder = 0;
 
 void setup()
 {
@@ -27,27 +30,47 @@ void setup()
 void sendTemps()
 {
  sensors.requestTemperatures();
- float temp = sensors.getTempCByIndex(0);
- Serial.println(temp);
+ temp = sensors.getTempCByIndex(0);
+ Serial.println(String("Sıcaklik=")+temp+ String(" C Uyari=")+MesajGonder+String(" EMail=")+EMailGonder);
  Blynk.virtualWrite(V1, temp);
+
  
+}
+
+BLYNK_READ(V2)
+{
+  Blynk.virtualWrite(V2,MesajGonder);
+}
+BLYNK_READ(V3)
+{
+  Blynk.virtualWrite(V3,EMailGonder);
+}
+
+BLYNK_WRITE(V2)
+{
+  MesajGonder = param.asInt(); 
+}
+
+BLYNK_WRITE(V3)
+{
+  EMailGonder = param.asInt(); 
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
  /* uyari */
- if ( (temp > 30 ) && (millis() > SonMesaj+3000 ) )
+ if ( (MesajGonder==true) &&  (temp > 30 ) && (millis() > SonMesaj+3000 ) )
  {
   SonMesaj = millis();
   Blynk.notify(String("Uyari ")+temp + String("C"));
  }
  /* Email */
- if ( (temp > 30 ) && (millis() > SonEMail+3000 ) )
+ if ( (EMailGonder==true)  && (temp > 30 ) && (millis() > SonEMail+3000 ) )
  {
   SonEMail = millis();
-  Blynk.email("gungor_yalcin68@Gmail.com","Blynk Deneme",String("Uyari ")+temp + String("C"));
+  Blynk.email("gungoryalcin68@Gmail.com","Blynk Deneme",String("Uyari ")+temp + String("C"));
  }
- 
-}
-void loop()
-{
-  Blynk.run();
-  timer.run();
-//  sendTemps();
+
 }
