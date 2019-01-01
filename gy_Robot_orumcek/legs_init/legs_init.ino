c06a4204b65427c6d71b1635c6d54887328b2cee
Gungor Yalcin <gungoryalcin68@gmail.com>
2019-01-01 18:07:16 +0300

    d
    d

---
+++ gy_Robot_orumcek/legs_init/legs_init.ino
@@ -0,0 +1,36 @@
+// Locate the initial position of legs 
+// RegisHsu 2015-09-09
+
+#include <Servo.h>   
+
+Servo servo[4][3];
+
+//define servos' ports
+const int servo_pin[4][3] = { {2, 3, 4}, {5, 6, 7}, {8, 9, 10}, {11, 12, 13} };
+
+void setup()
+{
+  //initialize all servos
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].attach(servo_pin[i][j]);
+      delay(20);
+    }
+  }
+}
+
+void loop(void)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].write(90);
+      delay(20);
+    }
+  }
+}
+
+
