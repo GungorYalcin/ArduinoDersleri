61539d3961d4b41a65e39af4e10d2c0e55c9381f
Gungor Yalcin <gungoryalcin68@gmail.com>
2019-01-01 21:57:40 +0300

    Update gy_nodemcu_DS18B20_blynk.ino
    1

--- gy_nodemcu_DS18B20_blynk/gy_nodemcu_DS18B20_blynk.ino
+++ gy_nodemcu_DS18B20_blynk/gy_nodemcu_DS18B20_blynk.ino
@@ -1,3 +1,6 @@
+/*
+ * gungor 01/01/2019
+*/
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
@@ -15,8 +18,6 @@ char pass[] = "5335921602";
 float  temp = 0;
 float SonMesaj = 0;
 float SonEMail = 0;
-int MesajGonder = 0;
-int EMailGonder = 0;
 
 void setup()
 {
@@ -31,46 +32,26 @@ void sendTemps()
 {
  sensors.requestTemperatures();
  temp = sensors.getTempCByIndex(0);
- Serial.println(String("Sıcaklik=")+temp+ String(" C Uyari=")+MesajGonder+String(" EMail=")+EMailGonder);
+ Serial.println(String("Sıcaklik=")+temp+ String(" C"));
  Blynk.virtualWrite(V1, temp);
-
- 
-}
-
-BLYNK_READ(V2)
-{
-  Blynk.virtualWrite(V2,MesajGonder);
-}
-BLYNK_READ(V3)
-{
-  Blynk.virtualWrite(V3,EMailGonder);
-}
-
-BLYNK_WRITE(V2)
-{
-  MesajGonder = param.asInt(); 
-}
-
-BLYNK_WRITE(V3)
-{
-  EMailGonder = param.asInt(); 
 }
 
 void loop()
 {
   Blynk.run();
   timer.run();
- /* uyari */
- if ( (MesajGonder==true) &&  (temp > 30 ) && (millis() > SonMesaj+3000 ) )
+ /* 
+ if ( (temp > 30 ) && (millis() > SonMesaj+3000 ) )
  {
   SonMesaj = millis();
   Blynk.notify(String("Uyari ")+temp + String("C"));
  }
- /* Email */
- if ( (EMailGonder==true)  && (temp > 30 ) && (millis() > SonEMail+3000 ) )
+ */
+ /*
+ if ( (temp > 30 ) && (millis() > SonEMail+3000 ) )
  {
   SonEMail = millis();
-  Blynk.email("gungoryalcin68@Gmail.com","Blynk Deneme",String("Uyari ")+temp + String("C"));
+  Blynk.email("xx8@Gmail.com","Blynk Deneme",String("Uyari ")+temp + String("C"));
  }
-
+*/
 }
