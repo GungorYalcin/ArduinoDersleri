e28fbd8f262a6cc652420da47c6152d53a34e0ca
Gungor Yalcin <gungoryalcin68@gmail.com>
2019-01-23 01:46:00 +0300

    Update gy_NodeMCU_Wifi_car_Blynk.ino
    1

--- gy_NodeMCU_Wifi_car_Blynk/gy_NodeMCU_Wifi_car_Blynk.ino
+++ gy_NodeMCU_Wifi_car_Blynk/gy_NodeMCU_Wifi_car_Blynk.ino
@@ -8,25 +8,32 @@
 // You should get Auth Token in the Blynk App.
 char auth[] = "e0430f3213b34c858b64938eac23a410"; //YourToken
 // Your WiFi credentials.
-char ssid[] = "lavender"; //YourNetworkName
-char pass[] = "5393789681"; //YourPassword
+char ssid[] = "Lavender"; //YourNetworkName
+char pass[] = "5335921602"; //YourPassword
 BlynkTimer timer;
-const byte L298N_A_pin = D1; // GPIO5
-const byte L298N_A_In1_pin = D2; // GPIO4
-const byte L298N_A_In2_pin = D3; // GPIO0
-const byte L298N_B_pin = D4; // GPIO2
-const byte L298N_B_In3_pin = D5; //GPIO14
-const byte L298N_B_In4_pin = D6; //GPIO12
+
+const byte L298N_A_pin = D1; 
+const byte L298N_A_In1_pin = D2; 
+const byte L298N_A_In2_pin = D3; 
+
+const byte L298N_B_In3_pin = D4; 
+const byte L298N_B_In4_pin = D5; 
+const byte L298N_B_pin = D6; 
+
 const byte Led1_pin =  D7; //GPIO13  // Sol Sinyal
-const byte Led2_pin =  D8; //GPIO15  // On Far
-const byte Led3_pin =  D9; //GPIO3   // Arka Lamba
-const byte Led4_pin = D10; //GPI01   // Sag Sinyal
+const byte Led2_pin =  D8; //GPIO15  // Sag Sinyal 
+const byte Led3_pin =  D9; //GPIO3   // On Far 
+const byte Led4_pin = D10; //GPI01   // Arka Lamba
 const byte Korna_pin = D0; //GPI016  //Korna
 
-WidgetLED led1(V1);
-WidgetLED led2(V2);
-WidgetLED led3(V3);
-WidgetLED led4(V4);
+WidgetLED led1(V1); //Sol
+WidgetLED led2(V2); //Sag
+WidgetLED led3(V3); //A1
+WidgetLED led4(V4); //B1
+WidgetLED led5(V5); //Far
+WidgetLED led6(V6); //Arka 
+WidgetLED led7(V7); //A2
+WidgetLED led8(V8); //B2
 
 byte SolSinyal = 0;
 byte SagSinyal = 0;
@@ -34,42 +41,51 @@ byte ArkaLamba = 0;
 /*
  * blynk ekranı
  * V0 joystick,  
- * v1,v2, v3, v4 Led  
- * v5  button push
- * v6 button switch
- * v7 gauge
-*/
+ * v1,v2, v3, v4,v5,v6,v7,v8 Led  
+ * v10  button push
+ * v11 button switch
+ */
 
-BLYNK_WRITE(V5) {
+BLYNK_WRITE(V10) {
 /* Korna*/  
   digitalWrite(Korna_pin,param.asInt());
 }
 
-BLYNK_WRITE(V6) {
+BLYNK_WRITE(V11) {
 /* On Far*/  
-  digitalWrite(Led2_pin,param.asInt());
+  digitalWrite(Led3_pin,param.asInt());
 }
 
-void motorSpeed(byte prmA, byte prmA1, byte prmA2, byte prmB, byte prmB1, byte prmB2)
+void motorSpeed(int prmA, byte prmA1, byte prmA2, int prmB, byte prmB1, byte prmB2)
 {
   analogWrite(L298N_A_pin,prmA);
+  analogWrite(L298N_B_pin,prmB);
+  
   digitalWrite(L298N_A_In1_pin,prmA1);
   digitalWrite(L298N_A_In2_pin,prmA2);
-  analogWrite(L298N_A_pin,prmB);
   digitalWrite(L298N_B_In3_pin,prmB1);
   digitalWrite(L298N_B_In4_pin,prmB2);
-  if ((prmA==0) && (prmB==0))
-  {
-    Blynk.virtualWrite(V7,0);
-  }
-   else if ((prmA==250) && (prmB==250))
-  {
-    Blynk.virtualWrite(V7,80);
-  }
+
+  if (prmA1==LOW) 
+    led3.off();
   else
-  {
-    Blynk.virtualWrite(V7,40);
-  }
+    led3.on();
+
+  if (prmA2==LOW) 
+    led7.off();
+  else
+    led7.on();
+
+  if (prmB1==LOW) 
+    led4.off();
+  else
+    led4.on();
+    
+  if (prmB2==LOW) 
+   led8.off();
+  else
+    led8.on();
+ 
 }
 
 
@@ -81,14 +97,16 @@ BLYNK_WRITE(V0) {
  if (y>=0)
  {
   ArkaLamba = 0;
+  digitalWrite(Led4_pin,LOW);
  }
  else
  {
   ArkaLamba = 1;
   SolSinyal = 1;
   SagSinyal = 1;
-   digitalWrite(Led1_pin,HIGH);
-   digitalWrite(Led4_pin,HIGH);
+  digitalWrite(Led1_pin,HIGH);
+  digitalWrite(Led2_pin,HIGH);
+  digitalWrite(Led4_pin,HIGH);
  }
  
  if ((x==0) && (y==0)) // duruyor
@@ -97,65 +115,60 @@ BLYNK_WRITE(V0) {
    SolSinyal = 0;
    SagSinyal = 0;
    digitalWrite(Led1_pin,LOW);
-   digitalWrite(Led4_pin,LOW);
+   digitalWrite(Led2_pin,LOW);
  }
  else if ((x==0) && (y>0)) // düz ileri 
  {
-  if (y==1){ motorSpeed(200,HIGH,LOW,200,HIGH,LOW); }
-  else { motorSpeed(250,HIGH,LOW,250,HIGH,LOW); }
+  if (y==1){ motorSpeed(900,HIGH,LOW,900,HIGH,LOW); }
+  else { motorSpeed(1023,HIGH,LOW,1023,HIGH,LOW); }
   SolSinyal = 0;
   SagSinyal = 0;
   digitalWrite(Led1_pin,LOW);
-  digitalWrite(Led4_pin,LOW);
+  digitalWrite(Led2_pin,LOW);
  }
  else if ((y==0) && (x>0))// Tam Sag donuş
  { 
-  motorSpeed(200,HIGH,LOW,200,LOW,HIGH); 
+  motorSpeed(900,HIGH,LOW,900,LOW,HIGH); 
   SagSinyal = 1;
-  digitalWrite(Led4_pin,HIGH);
+  digitalWrite(Led2_pin,HIGH);
   }
  else if ((y>0) && (x>0))// Sag donuş
  {  
-  motorSpeed(250,HIGH,LOW,200,HIGH,LOW); 
+  motorSpeed(1023,HIGH,LOW,900,LOW,LOW); 
   SagSinyal = 1;
-  digitalWrite(Led4_pin,HIGH);
+  digitalWrite(Led2_pin,HIGH);
   }
  else if ((y==0) && (x<0))// Tam sola donuş
  { 
-  motorSpeed(200,LOW,HIGH,200,HIGH,LOW); 
+  motorSpeed(900,LOW,HIGH,900,HIGH,LOW); 
   SolSinyal = 1;
   digitalWrite(Led1_pin,HIGH);
   }
- else if ((y>0) && (x>0))// Sola donuş
+ else if ((y>0) && (x<0))// Sola donuş
  {   
-  motorSpeed(200,LOW,HIGH,250,LOW,HIGH); 
+  motorSpeed(900,LOW,LOW,1023,HIGH,LOW); 
   SolSinyal = 1;
   digitalWrite(Led1_pin,HIGH);
   }
- else if ((y<0) && (x<0))// Sola geri
- {   motorSpeed(200,LOW,HIGH,250,LOW,HIGH); }
- else if ((y<0) && (x<0))// Sag geri
- {   motorSpeed(250,LOW,HIGH,200,LOW,HIGH); }
+  
+ if ((y<0) && (x<0))// Sola geri
+    motorSpeed(900,LOW,LOW,1023,LOW,HIGH); 
+ else if ((y<0) && (x>0))// Sag geri
+    motorSpeed(1023,LOW,HIGH,900,LOW,LOW); 
  else if ((y<0) && (x==0))// tam geri
- {   
-    motorSpeed(200,LOW,HIGH,200,LOW,HIGH); 
- }
+   motorSpeed(900,LOW,HIGH,900,LOW,HIGH); 
 
 }
 
 void setup() {
   Serial.begin(115200);
-  pinMode(L298N_A_pin,OUTPUT);
   pinMode(L298N_A_In1_pin,OUTPUT);
   pinMode(L298N_A_In2_pin,OUTPUT);
-  pinMode(L298N_B_pin,OUTPUT);
   pinMode(L298N_B_In3_pin,OUTPUT);
   pinMode(L298N_B_In4_pin,OUTPUT);
 
- digitalWrite(L298N_A_pin,LOW);
  digitalWrite(L298N_A_In1_pin,LOW);
  digitalWrite(L298N_A_In2_pin,LOW);
- digitalWrite(L298N_B_pin,LOW);
  digitalWrite(L298N_B_In3_pin,LOW);
  digitalWrite(L298N_B_In4_pin,LOW);
 
@@ -177,10 +190,10 @@ void sendTemps()
     digitalWrite(Led1_pin,!digitalRead(Led1_pin));
 
   if (SagSinyal==1)
-    digitalWrite(Led4_pin,!digitalRead(Led4_pin));
+    digitalWrite(Led2_pin,!digitalRead(Led2_pin));
 
   if (ArkaLamba==1)
-    digitalWrite(Led3_pin,!digitalRead(Led3_pin));
+    digitalWrite(Led4_pin,!digitalRead(Led4_pin));
 
   if (digitalRead(Led1_pin)==HIGH)
     led1.on();
@@ -193,20 +206,14 @@ void sendTemps()
     led2.off();
 
   if (digitalRead(Led3_pin)==HIGH)
-    led3.on();
+    led5.on();
   else
-    led3.off();
+    led5.off();
 
   if (digitalRead(Led4_pin)==HIGH)
-    led4.on();
+    led6.on();
   else
-    led4.off();
-    
-
-  //Blynk.virtualWrite(V1,digitalRead(Led1_pin));
-  //Blynk.virtualWrite(V2,digitalRead(Led2_pin));
-  //Blynk.virtualWrite(V3,digitalRead(Led3_pin));
-  //Blynk.virtualWrite(V4,digitalRead(Led4_pin));
+    led6.off();
 }
 
 
